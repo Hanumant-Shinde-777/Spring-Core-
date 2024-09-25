1f3622a71406da12945e7b1270bd9edc88fb9b0a
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-09-25 11:03:48 +0530

    Jounral Application Set up and file

---
+++ src/main/java/com/example/jounralApp/JounralApplication.java
@@ -0,0 +1,14 @@
+package com.example.jounralApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JounralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JounralApplication.class, args);
+	}
+
+}
+//Creating rest api using boot
